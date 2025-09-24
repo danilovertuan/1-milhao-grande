e3a75955394bf7f120d27e4459d25e8084794a52
danilovertuan <72980330+danilovertuan@users.noreply.github.com>
2025-09-24 17:39:17 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -18,12 +18,13 @@ import {
 
 // 🔹 Configuração Firebase
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "SEU_PROJECT.firebaseapp.com",
-  projectId: "SEU_PROJECT_ID",
-  storageBucket: "SEU_PROJECT.appspot.com",
-  messagingSenderId: "SEU_SENDER_ID",
-  appId: "SEU_APP_ID",
+  apiKey: "AIzaSyD92OfyBzYu-2KboRR9Jw4X8rFzniJEFzk",
+  authDomain: "milho-grande-e189a.firebaseapp.com",
+  projectId: "milho-grande-e189a",
+  storageBucket: "milho-grande-e189a.firebasestorage.app",
+  messagingSenderId: "825882602807",
+  appId: "1:825882602807:web:c8f2578567e74a1881beb2",
+  measurementId: "G-4JNE738GL7"
 };
 
 const app = initializeApp(firebaseConfig);
