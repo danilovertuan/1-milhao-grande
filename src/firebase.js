c7ea1be150bf629bbdb2cb83e0f12c37b3ba0323
danilovertuan <72980330+danilovertuan@users.noreply.github.com>
2025-09-26 18:55:24 -0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,5 @@
+// src/firebase.js
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
@@ -9,11 +9,7 @@ const firebaseConfig = {
   storageBucket: "milho-grande-e189a.firebasestorage.app",
   messagingSenderId: "825882602807",
   appId: "1:825882602807:web:c8f2578567e74a1881beb2",
-  measurementId: "G-4JNE738GL7"
 };
 
 const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
-export const provider = new GoogleAuthProvider();
 export const db = getFirestore(app);
